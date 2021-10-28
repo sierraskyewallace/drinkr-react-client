978b833bc9498ea9debb1b503f090de0299cef3e
sierraskyewallace <sskwallace@gmail.com>
2021-10-27 23:31:42 -0700

    adding drinks get request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,31 @@
 import React from 'react';
+import Drinks from './components/drinks';
 
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      drinks: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/api/v1/drinks")
+      .then(response => response.json())
+      .then(drinks => this.setState({ drinks }));
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <Drinks drinks={this.state.drinks} />
+      </div>
+    );
+  }
+}
 
 export default App;
